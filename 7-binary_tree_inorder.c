ae3b2d83a8ab7edbf6655884d85720a2fef264a3
ouidane <ouidanezakaria@gmail.com>
2024-12-04 17:20:16 +0100

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,3 +1,5 @@
+#include "binary_trees.h"
+
 /**
  * binary_tree_inorder - Traverses a binary tree in-order and
  * executes a given function on each node.
@@ -14,4 +16,3 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_inorder(tree->right, func);
 	}
 }
-
