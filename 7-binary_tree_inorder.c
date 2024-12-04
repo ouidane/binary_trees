6899782f5849272b3cecebfd30b6f0068abb5656
ouidane <ouidanezakaria@gmail.com>
2024-12-04 17:09:15 +0100

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+/**
+ * binary_tree_inorder - Traverses a binary tree in-order and
+ * executes a given function on each node.
+ *
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
+
