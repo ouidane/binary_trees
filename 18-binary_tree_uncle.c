e32a777d6483f7441fcd8be7ae6c3894805cfb60
ouidane <ouidanezakaria@gmail.com>
2024-12-04 18:02:42 +0100

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,10 +2,9 @@
 
 /**
  * binary_tree_uncle - Finds the uncle of a node.
- *
  * @node: The node to find the uncle of.
  *
- * Returns: The uncle of the node, or NULL if the node has no uncle.
+ * Return: The uncle of the node, or NULL.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
