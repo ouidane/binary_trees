fd31c37de79bf3e9dc38f76b8fb277de100dfbb0
ouidane <ouidanezakaria@gmail.com>
2024-12-04 18:02:41 +0100

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -21,14 +21,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int is_full_recursive(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (1);
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 ||
+		    is_full_recursive(tree->right) == 0)
 
-	if ((tree->left == NULL && tree->right != NULL) ||
-	    (tree->left != NULL && tree->right == NULL) ||
-	    is_full_recursive(tree->left) == 0 ||
-	    is_full_recursive(tree->right) == 0)
-		return (0);
+			return (0);
+	}
 
 	return (1);
 }
