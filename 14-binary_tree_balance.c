f3754b20191d5170bcf89e38f040b975a78c234d
ouidane <ouidanezakaria@gmail.com>
2024-12-04 17:46:21 +0100

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Calculates the balance factor of a binary tree.
+ * @tree: The tree to calculate the balance factor of.
+ *
+ * Return: The balance factor of the tree, or 0 if the tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - Calculates the height of a binary tree.
+ * @tree: The tree to calculate the height of.
+ *
+ * Return: The height of the tree, or 0 if the tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
+
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+
+		return ((lef > rig) ? lef : rig);
+	}
+
+	return (0);
+}
