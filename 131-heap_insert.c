a823a991e6480379d988ce47fe928eaf76aab99f
ouidane <ouidanezakaria@gmail.com>
2024-12-06 00:43:21 +0100

    task 37

---
+++ 131-heap_insert.c
@@ -0,0 +1,59 @@
+#include "binary_trees.h"
+
+heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/**
+ * heap_insert - Insert a value in a Max Binary Heap.
+ * @root: The root of the tree where we will insert the new node.
+ * @value: The value to store in the new node.
+ *
+ * Return: The pointer to the new node, or NULL on failure.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tree, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+
+	tree = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leaves & bit ? tree->right : tree->left;
+
+	new = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new) : (tree->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+		new = new->parent;
+	}
+
+	return (new);
+}
+
+/**
+ * binary_tree_size - Counts the number of nodes in a binary tree.
+ * @tree: The tree to count.
+ *
+ * Return: The number of nodes in the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
